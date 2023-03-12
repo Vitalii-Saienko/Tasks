31e75abf085d307e6315613058c720a899a505a8
Vitalii <vitaliy5saie@gmail.com>
2023-03-12 19:40:58 +0100

    added hometask fill array in a spiral

---
+++ src/Practise/SpiralFillArray.java
@@ -0,0 +1,63 @@
+package Practise;
+
+import java.util.Scanner;
+
+/*
+Write a method that creates an array of given length N and fills it in a spiral from 1 to N*N
+ */
+
+public class SpiralFillArray {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int arraySize = scanner.nextInt();
+        scanner.close();
+        int[][] arr = new int[(int) Math.sqrt(arraySize)][(int) Math.sqrt(arraySize)];
+
+        int counter = 1;
+        int beginIndex = 0;
+        int endIndex = (int) (Math.sqrt(arraySize)-1);
+
+        while (counter!=arraySize) {
+
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[beginIndex][j] == 0) {
+                    arr[beginIndex][j] = counter;
+                    counter++;
+                }
+            }
+
+            for (int i = beginIndex; i < arr.length; i++) {
+                if (arr[i][endIndex] == 0) {
+                    arr[i][endIndex] = counter;
+                    counter++;
+                }
+            }
+
+            for (int j = endIndex; j >= 0; j--) {
+                if (arr[endIndex][j] == 0) {
+                    arr[endIndex][j] = counter;
+                    counter++;
+                }
+            }
+
+            for (int i = endIndex; i >= 0; i--) {
+                if (arr[i][beginIndex] == 0) {
+                    arr[i][beginIndex] = counter;
+                    counter++;
+                }
+            }
+            beginIndex++;
+            endIndex--;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (arr[i][j] == 0){
+                    arr[i][j] = arraySize;
+                }
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
